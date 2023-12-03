f7e404aeb523ca38ee2f0c2d64b2d37f236138d8
fringemonkey89 <markomajeticjr@gmail.com>
2023-12-03 17:23:14 -0500

    added initial files

---
+++ music-player/src/component/Player.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from 'react'
+import useSound from 'use-sound';
+import qala from '../assets/qala.mp3'
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"
+import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"
+import { IconContext } from "react-icons"
